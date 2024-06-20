ec2cd34719e9d3ad32e9e3068c0323a2ff20f5af
karanhudia <karanhudia@grover.com>
2024-06-20 21:33:13 +0530

    feat: add content component

--- src/components/LottieViewer.tsx
+++ src/components/LottieViewer.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { DotLottie, DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { SharedProps } from '../context/SharedPropsContext';
+import { Stack } from '@mui/joy';
 
 export const LottieViewer = () => {
   const { setDotLottie, lottieJSON } = useContext(SharedProps);
@@ -10,11 +11,13 @@ export const LottieViewer = () => {
   };
 
   return (
-    <DotLottieReact
-      data={JSON.stringify(lottieJSON)}
-      loop
-      autoplay
-      dotLottieRefCallback={dotLottieRefCallback}
-    />
+    <Stack spacing={2} sx={{ px: { xs: 2, md: 4 }, pt: 2, minHeight: 0 }}>
+      <DotLottieReact
+        data={JSON.stringify(lottieJSON)}
+        loop
+        autoplay
+        dotLottieRefCallback={dotLottieRefCallback}
+      />
+    </Stack>
   );
 };
