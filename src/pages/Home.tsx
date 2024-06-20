e34584c122dc3b71cbd8381b7780db363cd77527
karanhudia <karanhudia@grover.com>
2024-06-21 00:18:15 +0530

    chore: add basic styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,11 @@ import { FileUpload } from '../components/FileUpload';
 
 export const Home = () => {
   return (
-    <Box alignContent='center' alignItems='center'>
-      <Typography>Welcome to Home</Typography>
-      <FileUpload />
+    <Box alignContent='center' alignItems='center' width='100%' sx={{ height: '100%' }}>
+      <Typography level='h2'>Welcome to Home</Typography>
+      <Box pt={2}>
+        <FileUpload />
+      </Box>
     </Box>
   );
 };
