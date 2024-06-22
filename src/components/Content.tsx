7e3d28370bdde927430400196aa43a69f7733f63
karanhudia <karanhudia@grover.com>
2024-06-22 12:34:24 +0530

    chore: refactor Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Box } from '@mui/joy';
-import { LottieViewer } from './LottieViewer';
+import { styled } from '@mui/joy';
 import { EditControls } from './EditControls';
-import { LayersControl } from './LayersControl';
+import { SideBar } from './SideBar';
+import { LottieViewer } from './LottieViewer';
+
+const ContentWrapper = styled('div')`
+  display: grid;
+  height: calc(100% - 55px);
+  grid-template-columns: 25% 50% 25%;
+`;
 
 export const Content = () => {
   return (
-    <Box
-      component='main'
-      sx={{
-        height: 'calc(100vh - 55px)', // 55px is the height of the NavBar
-        display: 'grid',
-        gridTemplateColumns: { xs: 'auto', md: '20% 50% 30%' },
-        gridTemplateRows: 'auto 1fr auto',
-      }}
-    >
-      <LayersControl />
+    <ContentWrapper>
+      <SideBar />
       <LottieViewer />
       <EditControls />
-    </Box>
+    </ContentWrapper>
   );
 };
