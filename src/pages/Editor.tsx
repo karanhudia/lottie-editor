0cbf0f2ebfd84980245ba11a19767d91e52cf149
karanhudia <karanhudia@grover.com>
2024-06-23 18:28:50 +0530

    chore: add null check for editor

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -4,10 +4,14 @@ import { Content } from '../components/Content';
 import { SharedProps } from '../context/SharedPropsContext';
 import { useParams } from 'react-router-dom';
 import { useFetchEditedLottieQuery } from '../graphql/generated/graphql';
+import { useSocket } from '../hooks/useSocket';
+
+export type EditorRouteParams = { editId: string };
 
 export const Editor = () => {
+  useSocket();
   const { setLottieJSON } = useContext(SharedProps);
-  const params = useParams<{ editId: string }>();
+  const params = useParams<EditorRouteParams>();
 
   useFetchEditedLottieQuery({
     variables: {
@@ -15,7 +19,11 @@ export const Editor = () => {
     },
     fetchPolicy: 'no-cache',
     onCompleted: (data) => {
-      setLottieJSON(data.lottie?.json);
+      if (!data.lottie) {
+        return;
+      }
+
+      setLottieJSON(data.lottie.json);
     },
   });
 
