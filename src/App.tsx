a8dc3836808f0e58170301c2eb049da3fe506402
karanhudia <karanhudia@grover.com>
2024-06-23 18:17:05 +0530

    chore: remove socket info from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import '@fontsource/inter';
 import { CssVarsProvider } from '@mui/joy';
 import { ApolloProvider } from '@apollo/client';
 import { SharedPropsContext } from './context/SharedPropsContext';
-import { socket } from './socket';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Editor } from './pages/Editor';
@@ -12,26 +11,6 @@ import { Error } from './pages/Error';
 import { client } from './graphql/client';
 
 function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-    };
-  }, []);
-
   const router = createHashRouter([
     {
       path: '/',
@@ -44,8 +23,6 @@ function App() {
       errorElement: <Error />,
     },
   ]);
-
-  console.info({ isConnected });
 
   return (
     <div className='App'>
