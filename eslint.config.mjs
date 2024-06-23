e33c15ea7e0dedfb9c3cb0e7223c17140977a054
karanhudia <karanhudia@grover.com>
2024-06-23 11:39:00 +0530

    chore: add fetch lottie query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import graphqlLint from '@graphql-eslint/eslint-plugin';
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.strictTypeChecked,
     ...tseslint.configs.stylisticTypeChecked,
     ...tseslint.configs.recommendedTypeChecked,
+    ...graphqlLint,
     {
         languageOptions: {
             parserOptions: {
@@ -15,7 +17,7 @@ export default tseslint.config(
         },
     },
     {
-        files: ['eslint.config.mjs'],
+        files: ['eslint.config.mjs', 'graphql.ts'],
         ...tseslint.configs.disableTypeChecked,
     },
     {
