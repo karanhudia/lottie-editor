7556c08808fc5d2a3a612ea623e5fb2ed2582de1
karanhudia <karanhudia@grover.com>
2024-06-23 18:16:34 +0530

    chore: remove graphql lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,11 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import graphqlLint from '@graphql-eslint/eslint-plugin';
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.strictTypeChecked,
     ...tseslint.configs.stylisticTypeChecked,
     ...tseslint.configs.recommendedTypeChecked,
-    ...graphqlLint,
     {
         languageOptions: {
             parserOptions: {
