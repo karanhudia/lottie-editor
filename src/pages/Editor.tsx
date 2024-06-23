6a3e9b8c0ed6ac0656f77c224f9fd4fcdc425acb
karanhudia <karanhudia@grover.com>
2024-06-23 11:32:49 +0530

    chore: use graphql hook to fetch edit file

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,37 +1,23 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Header } from '../components/Header';
 import { Content } from '../components/Content';
 import { SharedProps } from '../context/SharedPropsContext';
-import { useLoaderData } from 'react-router-dom';
-import { LottieAnimation } from '../types/LottieAnimation';
-
-const fetchLottie = async () => {
-  try {
-    // TODO: Fetch data from BACKEND
-    const response = await fetch(
-      'https://assets-v2.lottiefiles.com/a/3c01a012-27ca-11ef-a9a3-53cd7688f762/fvgzs9fjrn.json',
-    );
-    return (await response.json()) as LottieAnimation;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export async function loader() {
-  const lottie = await fetchLottie();
-
-  return { lottie };
-}
+import { useParams } from 'react-router-dom';
+import { useFetchEditedLottieQuery } from '../graphql/generated/graphql';
 
 export const Editor = () => {
-  const { lottie } = useLoaderData() as { lottie: LottieAnimation };
-
   const { setLottieJSON } = useContext(SharedProps);
+  const params = useParams<{ editId: string }>();
 
-  useEffect(() => {
-    console.info('Fetched Lottie', lottie);
-    setLottieJSON(lottie);
-  }, [lottie, setLottieJSON]);
+  useFetchEditedLottieQuery({
+    variables: {
+      editId: params.editId ?? '',
+    },
+    fetchPolicy: 'no-cache',
+    onCompleted: (data) => {
+      setLottieJSON(data.lottie?.json);
+    },
+  });
 
   return (
     <>
