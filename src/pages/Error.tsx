9ff538ba39e55e2e718241ce0cc6781c924c27ae
karanhudia <karanhudia@grover.com>
2024-06-25 20:29:31 +0530

    chore: styling error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { Box, Typography } from '@mui/joy';
+import { InfoOutlined } from '@mui/icons-material';
 
 export const Error = () => {
   const error = useRouteError();
@@ -16,12 +18,35 @@ export const Error = () => {
   console.error(error);
 
   return (
-    <div id='error-page'>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{errorMessage}</i>
-      </p>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        backgroundColor: 'danger.100',
+        gap: 2,
+        height: '100%',
+      }}
+    >
+      <Typography
+        level='h1'
+        color='danger'
+        startDecorator={<InfoOutlined />}
+        sx={{ alignItems: 'flex-start', maxWidth: 240, wordBreak: 'break-all' }}
+      >
+        {errorMessage}
+      </Typography>
+      <Typography
+        variant='solid'
+        color='danger'
+        startDecorator='🚨'
+        fontSize='lg'
+        sx={{ '--Typography-gap': '0.5rem', p: 1, borderRadius: 5 }}
+      >
+        Sorry, an unexpected error has occurred
+      </Typography>
+    </Box>
   );
 };
