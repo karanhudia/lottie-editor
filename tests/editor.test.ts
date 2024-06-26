b66b67bc77814ab36a14c449a4fc1e97f7a7a46c
karanhudia <karanhudia@grover.com>
2024-06-26 13:49:10 +0530

    fix: playwright tests

--- tests/editor.test.ts
+++ tests/editor.test.ts
@@ -4,13 +4,13 @@ test.describe('editor page', () => {
   test.beforeEach(async ({ page }) => {
     // Go to the starting url before each test.
     await page.goto(
-      'http://localhost:3000/lottie-editor/random#/edit/4237dad5-98a6-4dbd-b304-0d5b6494efe7',
+      'http://localhost:3000/lottie-editor/#/edit/4237dad5-98a6-4dbd-b304-0d5b6494efe7',
     );
   });
 
   test('navigation', async ({ page }) => {
     await expect(page).toHaveURL(
-      'http://localhost:3000/lottie-editor/random#/edit/4237dad5-98a6-4dbd-b304-0d5b6494efe7',
+      'http://localhost:3000/lottie-editor/#/edit/4237dad5-98a6-4dbd-b304-0d5b6494efe7',
     );
 
     await expect(page).toHaveTitle(/Lottie Editor/);
@@ -23,12 +23,11 @@ test.describe('editor page', () => {
 
   test('editing controls', async ({ page }) => {
     await expect(page.getByText('Speed')).toBeVisible();
-    await expect(page.getByText('Scale')).toBeVisible();
+    await expect(page.getByText('Share Lottie')).toBeVisible();
     await expect(page.getByText('Export JSON')).toBeVisible();
 
-    await expect(page.getByRole('radiogroup').locator('span').nth(2)).toBeVisible();
-    await page.getByRole('radiogroup').locator('span').nth(2).click();
-    await expect(page.locator('div:nth-child(5) > div:nth-child(3)')).toBeVisible();
+    await expect(page.locator('div:nth-child(5) > div').first()).toBeVisible();
+    await page.locator('[id="\\:r3\\:"]').check();
     await page.getByLabel('Color').locator('div').first().click();
   });
 });
